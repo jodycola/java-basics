204df0616bc28bad086d00c3c8009691c4c3a285
Kody Samaroo <github email address>
2021-06-18 10:38:33 -0400

    basic strings done

---
+++ Strings.java
@@ -0,0 +1,17 @@
+public class Strings {
+    
+    public static void main(String[] args) {
+        int num = 9;
+        double aDouble = 9.5;
+
+
+        String text = "Hello World";
+        String name = "Jody";
+        String greeting = "Hello";
+
+        System.out.println(text);
+        System.out.println(greeting + " " + name);
+        System.out.println("My numer: " + num + " & " + aDouble);
+
+    }
+}
