415bd3f7f2630a41d172699c5b3f66d7fe48612b
Kody Samaroo <github email address>
2021-06-18 10:33:47 -0400

    basic variables

---
+++ Variables.java
@@ -0,0 +1,24 @@
+public class Variables {
+    
+    public static void main(String[] args) {
+        int aValue = 42;
+        short aShort = 100;
+        long aLong = 5000;
+        double aDouble = 3.5432;
+        float aFloat = 5.234f;
+
+        char aChar = 'a';
+        boolean aBoolean = true;
+
+        System.out.println(aValue);
+        System.out.println(aShort);
+        System.out.println(aLong);
+        System.out.println(aDouble);
+        System.out.println(aFloat);
+        System.out.println(aChar);
+        System.out.println(aBoolean);
+        
+
+
+    }
+}
