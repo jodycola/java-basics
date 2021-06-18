41e0ad296a98e2a94c1538b87b14d98af61121f1
Kody Samaroo <github email address>
2021-06-18 12:12:53 -0400

    while loop done

---
+++ Loops.java
@@ -0,0 +1,11 @@
+public class Loops {
+    
+    public static void main(String[] args) {
+        int num = 0;
+
+        while(num < 10) {
+            num = num + 1;
+            System.out.println(num);
+        }
+    }
+}
