9aebd2a4df9decbbf96e1caaaa1f53e392097bb1
Kody Samaroo <github email address>
2021-06-18 12:53:16 -0400

    basic For Loops done

---
+++ ForLoops.java
@@ -0,0 +1,8 @@
+public class ForLoops {
+    
+    public static void main(String[] args) {
+        for ( int i = 1; i <= 10; i++){
+            System.out.printf("The variable of i is %d\n", i);
+        }
+    }
+}
